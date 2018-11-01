bf47d81077cd3d89bfb3874e4bcf8178e1108326
Tristan Kuehn <tristankuehn@gmail.com>
2018-11-01 13:59:14 -0400

    First attempt at drawing out the stats
    Still need to structure and save them, and there are questions about their
    accuracy.

--- src/crossing_angle.m
+++ src/crossing_angle.m
@@ -1,17 +1,56 @@
-layer = 6
 
-disp_voxel_data = niftiread("dti_201_scan1_3dPrintPhantomBottom6_disp_voxel_data.nii.gz");
-disp_voxel_dir = niftiread("dti_201_scan1_3dPrintPhantomBottom6_disp_voxel_dir.nii.gz");
+base_file = 'dti_201_scan2_3dPrintPhantomTop6';
 
-disp_voxel_data_2 = disp_voxel_data(:, :, :, 1:2);
-disp_voxel_dir_2 = disp_voxel_dir(:, :, :, 1:6);
+disp_voxel_data = niftiread([base_file '_disp_voxel_data.nii.gz']);
+disp_voxel_dir = niftiread([base_file '_disp_voxel_dir.nii.gz']);
+afd_voxel_data = niftiread([base_file '_afd_voxel_data.nii.gz']);
+peak_voxel_data = niftiread([base_file '_peak_voxel_data.nii.gz']);
 
-top_dir_1 = num2cell(squeeze(disp_voxel_dir(:, :, layer, 1:2)), 3);
-top_dir_1 = cellfun(@(x) squeeze(x), top_dir_1, 'UniformOutput', false);
+% Discard all but the two most significant fixels
+disp_voxel_data = disp_voxel_data(:, :, :, 1:2);
+disp_voxel_dir = disp_voxel_dir(:, :, :, 1:6);
+% Split into a cell array, where the 4th dimension is two vectors
+disp_voxel_dir = cat(4, num2cell(disp_voxel_dir(:, :, :, 1:3), 4), ...
+    num2cell(disp_voxel_dir(:, :, :, 4:6), 4));
 
-top_dir_2 = num2cell(squeeze(disp_voxel_dir(:, :, layer, 4:5)), 3);
-top_dir_2 = cellfun(@(x) squeeze(x), top_dir_2, 'UniformOutput', false);
+layers = size(disp_voxel_data, 3);
 
-cross = cellfun(@(x, y) acos(dot(x, y) / (norm(x) * norm(y))), top_dir_1, top_dir_2);
+% Initialize structures for summary statistics
+mean_cross = zeros([layers 1]);
+std_cross = zeros([layers 1]);
+mean_disp = zeros([layers 1]);
+std_disp = zeros([layers 1]);
+mean_afd = zeros([layers 1]);
+std_afd = zeros([layers 1]);
+mean_peak = zeros([layers 1]);
+std_peak = zeros([layers 1]);
 
-cross(48, 30) / pi
+% Loop through every layer
+for layer = 1:layers
+    % First, find crossing angles
+    % Make cell array of direction vectors
+    fixel_dir_1 = cellfun(@(x) squeeze(x), ...
+        squeeze(disp_voxel_dir(:, :, layer, 1)), 'UniformOutput', false);
+    fixel_dir_2 = cellfun(@(x) squeeze(x), ...
+        squeeze(disp_voxel_dir(:, :, layer, 2)), 'UniformOutput', false);
+    
+    % Norm of pixels with no phantom will be zero, becoming NaN here
+    cross = cellfun(@(x, y) acos(dot(x, y) / (norm(x) * norm(y))), ...
+        fixel_dir_1, fixel_dir_2);
+    
+    mean_cross(layer) = mean(cross(not(isnan(cross))));
+    std_cross(layer) = std(cross(not(isnan(cross))));
+    
+    layer_disp = squeeze(disp_voxel_data(:, :, layer, :));
+    mean_disp(layer) = mean(layer_disp(layer_disp > 0));
+    std_disp(layer) = std(layer_disp(layer_disp > 0));
+    
+    layer_afd = squeeze(afd_voxel_data(:, :, layer, :));
+    mean_afd(layer) = mean(layer_afd(layer_afd > 0));
+    std_afd(layer) = std(layer_afd(layer_afd > 0));
+    
+    layer_peak = squeeze(peak_voxel_data(:, :, layer, :));
+    mean_peak(layer) = mean(layer_peak(layer_peak > 0));
+    std_peak(layer) = std(layer_peak(layer_peak > 0));
+end
+
